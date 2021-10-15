1529cc70bb20db10e5c64d29de7bf88ce81a8812
Lucas Amaral <lu.vinicius01@gmail.com>
2021-10-15 15:49:30 -0300

    add number soap requests

---
+++ number/number_to_words.rb
@@ -0,0 +1,16 @@
+require 'savon'
+
+client = Savon.client(
+  wsdl: 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL'
+)
+
+client.operations
+
+response = client.call(:number_to_words, message: { ubi_num: 500 })
+
+if response.success?
+  puts response.body
+else
+  puts response.xml.to_s
+  puts response.http_error
+end
